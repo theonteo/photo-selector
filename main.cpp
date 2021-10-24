6d1864091f48fedad056a19c4d24cc32c191aed8
theonteo <56335249+theonteo@users.noreply.github.com>
2021-10-24 18:03:06 +0800

    Init push

---
+++ main.cpp
@@ -0,0 +1,23 @@
+
+#include <QApplication>
+#include <QCommandLineParser>
+
+#include "imageviewer.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QGuiApplication::setApplicationDisplayName(ImageViewer::tr("Image Viewer"));
+    QCommandLineParser commandLineParser;
+
+    commandLineParser.addHelpOption();
+    commandLineParser.addPositionalArgument(ImageViewer::tr("[file]"), ImageViewer::tr("Image file to open."));
+    commandLineParser.process(QCoreApplication::arguments());
+    ImageViewer imageViewer;
+    if (!commandLineParser.positionalArguments().isEmpty()
+        && !imageViewer.loadFile(commandLineParser.positionalArguments().constFirst())) {
+        return -1;
+    }
+    imageViewer.show();
+    return app.exec();
+}
